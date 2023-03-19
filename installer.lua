d49cb125c5833bf7452552fb624818b22661312f
NeroOneTrueKing <azure@azenweb.org>
2023-03-19 10:54:38 -0500

    Add installer

---
+++ installer.lua
@@ -0,0 +1,22 @@
+local shell = require("shell")
+local filesystem = require("filesystem")
+local scripts = {
+    "pwr.lua",
+    "redstone-control.lua",
+    "installer.lua"
+}
+
+local function exists(filename)
+    return filesystem.exists(shell.getWorkingDirectory().."/"..filename)
+end
+
+local repo = "http://github.com/NeroOneTrueKing/GTNH-Power/";
+local branch = "main"
+
+for i=1, #scripts do
+    shell.execute(string.format("wget -f %s%s/%s", repo, branch, scripts[i]));
+end
+
+if not exists("config.lua") then
+    shell.execute(string.format("wget %s%s/config.lua", repo, branch));
+end
